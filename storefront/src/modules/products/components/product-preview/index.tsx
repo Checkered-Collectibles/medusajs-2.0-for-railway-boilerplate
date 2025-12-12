a2126acf4e9a9f2e679e0b3e5445e94adc51976f
Shubhankar <shubhankartrivedi1234@gmail.com>
2025-12-12 22:30:37 +0530

    new add to cart button

--- storefront/src/modules/products/components/product-preview/index.tsx
+++ storefront/src/modules/products/components/product-preview/index.tsx
@@ -6,6 +6,7 @@ import Thumbnail from "../thumbnail"
 import PreviewPrice from "./price"
 import { getProductsById } from "@lib/data/products"
 import { HttpTypes } from "@medusajs/types"
+import QuickAddToCartButton from "../product-preview-instant/add-to-cart"
 
 export default async function ProductPreview({
   product,
@@ -28,25 +29,30 @@ export default async function ProductPreview({
   const { cheapestPrice } = getProductPrice({
     product: pricedProduct,
   })
-
-  return (
-    <LocalizedClientLink href={`/products/${product.handle}`} className="group">
-      <div data-testid="product-wrapper">
-        <Thumbnail
-          thumbnail={product.thumbnail}
-          images={product.images}
-          size="full"
-          isFeatured={isFeatured}
-        />
-        <div className="flex txt-compact-medium mt-4 justify-between">
-          <Text className="text-ui-fg-subtle" data-testid="product-title">
-            {product.title}
-          </Text>
-          <div className="flex items-center gap-x-2">
-            {cheapestPrice && <PreviewPrice price={cheapestPrice} />}
+  if (region.countries && region.countries[0].iso_2 && product.variants)
+    return (
+      <div className="flex flex-col gap-5 group/item justify-between h-full">
+        <LocalizedClientLink href={`/products/${product.handle}`} className="group">
+          <div data-testid="product-wrapper">
+            <Thumbnail
+              thumbnail={product.thumbnail}
+              images={product.images}
+              size="full"
+              isFeatured={isFeatured}
+            />
+            <div className="flex txt-compact-medium mt-4 justify-between">
+              <Text className="text-ui-fg-subtle" data-testid="product-title">
+                {product.title}
+              </Text>
+              <div className="flex items-center gap-x-2">
+                {cheapestPrice && <PreviewPrice price={cheapestPrice} />}
+              </div>
+            </div>
           </div>
+        </LocalizedClientLink>
+        <div className="md:invisible md:group-hover/item:visible">
+          <QuickAddToCartButton productId={product.id} countryCode={region.countries[0].iso_2} variant={product.variants[0]} />
         </div>
       </div>
-    </LocalizedClientLink>
-  )
+    )
 }
