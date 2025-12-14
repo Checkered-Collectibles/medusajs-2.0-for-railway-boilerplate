de01df73a9746b914eb0d589986d7e7852ad6fc6
Shubhankar <shubhankartrivedi1234@gmail.com>
2025-12-14 20:09:16 +0530

    improved whatsapp

--- storefront/src/modules/layout/templates/whatsapp/index.tsx
+++ storefront/src/modules/layout/templates/whatsapp/index.tsx
@@ -1,12 +1,16 @@
 import Image from "next/image"
-import WhatsappLogo from "@images/whatsapp.png"
+import WhatsappLogo from "@images/whatsapp/logo.png";
+import Qr from "@images/whatsapp/qr.png";
 import Link from "next/link"
 
 export default function WhatsappContact() {
     return (
         <div className="fixed bottom-0 right-0 p-5 z-50">
             <Link href="https://chat.whatsapp.com/D09sAJ51EXX5MzuFOAmAAK?mode=hqrt2" title="Join our Whatsapp" target="_blank" className="">
-                <Image src={WhatsappLogo} alt="whatsapp" width={70} className="bg-green-100 p-2 rounded-3xl rounded-bl-none" />
+                <div className="bg-green-100 p-2 rounded-3xl space-y-3 border border-black/10 shadow-lg">
+                    <Image src={WhatsappLogo} alt="whatsapp" width={70} className="" />
+                    <Image src={Qr} alt="whatsapp qr" width={70} className="rounded-2xl hidden md:block" />
+                </div>
             </Link>
         </div>
     )
