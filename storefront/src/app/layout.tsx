9af497084434bf538f1df38ceb62b6ed8d3089af
Shubhankar <shubhankartrivedi1234@gmail.com>
2025-12-16 22:07:37 +0530

    removed meta pixel

--- storefront/src/app/layout.tsx
+++ storefront/src/app/layout.tsx
@@ -12,52 +12,18 @@ export const metadata: Metadata = {
 export default function RootLayout(props: { children: React.ReactNode }) {
   return (
     <html lang="en" data-mode="light">
+
       <body className={`${inter.variable} font-sans`}>
-        {/* Google Ads / gtag */}
-        <Script
-          src="https://www.googletagmanager.com/gtag/js?id=AW-17801513380"
-          strategy="afterInteractive"
-        />
+        <Script src="https://www.googletagmanager.com/gtag/js?id=AW-17801513380" strategy="afterInteractive" />
         <Script id="google-analytics" strategy="afterInteractive">
           {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){window.dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'AW-17801513380');
-          `}
+				window.dataLayer = window.dataLayer || [];
+				function gtag(){window.dataLayer.push(arguments);}
+				gtag('js', new Date());
+
+				gtag('config', 'AW-17801513380');
+				`}
         </Script>
-
-        {/* Meta Pixel */}
-        <Script
-          id="meta-pixel"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
-              !function(f,b,e,v,n,t,s)
-              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-              n.queue=[];t=b.createElement(e);t.async=!0;
-              t.src=v;s=b.getElementsByTagName(e)[0];
-              s.parentNode.insertBefore(t,s)}(window, document,'script',
-              'https://connect.facebook.net/en_US/fbevents.js');
-              fbq('init', '1273773987893981');
-              fbq('track', 'PageView');
-            `,
-          }}
-        />
-
-        {/* Meta Pixel noscript fallback */}
-        <noscript>
-          <img
-            height="1"
-            width="1"
-            style={{ display: "none" }}
-            src="https://www.facebook.com/tr?id=1273773987893981&ev=PageView&noscript=1"
-            alt=""
-          />
-        </noscript>
-
         <main className="relative">{props.children}</main>
       </body>
     </html>
