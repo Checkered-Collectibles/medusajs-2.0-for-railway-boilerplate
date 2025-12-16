8435e80d72240f41db744acc27eca3e7522be181
Shubhankar <shubhankartrivedi1234@gmail.com>
2025-12-16 15:20:40 +0530

    images optimized

--- storefront/src/middleware.ts
+++ storefront/src/middleware.ts
@@ -149,6 +149,6 @@ export async function middleware(request: NextRequest) {
 
 export const config = {
   matcher: [
-    "/((?!api|_next/static|favicon.ico|.*\\.png|.*\\.jpg|.*\\.gif|.*\\.svg).*)",
-  ], // prevents redirecting on static files
+    "/((?!api|_next/static|_next/image|favicon.ico|robots.txt|sitemap.xml).*)",
+  ],
 }
