fde9f59a2f5378b2b0d88b8579928afaa661c1ec
Shubhankar <shubhankartrivedi1234@gmail.com>
2025-12-18 19:55:27 +0530

    whatsapp button layer fixed

--- storefront/src/modules/layout/templates/whatsapp/index.tsx
+++ storefront/src/modules/layout/templates/whatsapp/index.tsx
@@ -5,7 +5,7 @@ import Link from "next/link"
 
 export default function WhatsappContact() {
     return (
-        <div className="fixed bottom-0 right-0 p-5 z-50">
+        <div className="fixed bottom-0 right-0 p-5 z-30">
             <Link href="https://chat.whatsapp.com/D09sAJ51EXX5MzuFOAmAAK?mode=hqrt2" title="Join our Whatsapp" target="_blank" className="">
                 <div className="bg-green-100 p-2 rounded-3xl space-y-3 border border-black/10 shadow-lg">
                     <Image src={WhatsappLogo} alt="whatsapp" width={70} className="" />
