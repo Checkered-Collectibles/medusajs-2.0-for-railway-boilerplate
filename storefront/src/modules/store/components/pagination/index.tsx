934ed233397be09304936205a76b5cf86db868a6
Shubhankar <shubhankartrivedi1234@gmail.com>
2025-12-18 19:54:04 +0530

    better pagination ui

--- storefront/src/modules/store/components/pagination/index.tsx
+++ storefront/src/modules/store/components/pagination/index.tsx
@@ -35,8 +35,8 @@ export function Pagination({
   ) => (
     <button
       key={p}
-      className={clx("txt-xlarge-plus text-ui-fg-muted", {
-        "text-ui-fg-base hover:text-ui-fg-subtle": isCurrent,
+      className={clx("txt-xlarge-plus text-ui-fg-muted h-10 w-10 rounded-full bg-black/0", {
+        "text-ui-fg-base hover:text-ui-fg-subtle bg-black/10": isCurrent,
       })}
       disabled={isCurrent}
       onClick={() => handlePageChange(p)}
