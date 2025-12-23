21442a5c44d1de469eb3220199eafcf4072b7e2d
Shubhankar <shubhankartrivedi1234@gmail.com>
2025-12-24 01:22:21 +0530

    exotic envy coming soon

--- storefront/src/modules/home/components/hero/graphic.tsx
+++ storefront/src/modules/home/components/hero/graphic.tsx
@@ -4,6 +4,8 @@ import Image from "next/image";
 import HeroImage1 from "@images/ferrari-hero.png";
 import HeroImage2 from "@images/bugatti-hero.png";
 import EuroSpeed from "@images/euro-speed.png";
+import ExoticEnvy from "@images/exotic-envy.jpg";
+
 import gsap from "gsap";
 import { useGSAP } from "@gsap/react";
 import { useRef } from "react";
@@ -39,17 +41,31 @@ export default function HeroGraphic() {
                 alt="Hero image 2"
                 className="hero-img drop-shadow-2xl scale-x-[-1]"
             />
-            <div title="Hot Wheels Premium Euro Speed set coming soon" className="hero-img absolute bottom-0 left-0 bg-white border shadow-md rounded-3xl m-2 p-2 flex flex-col items-center">
-                <Image
-                    src={EuroSpeed}
-                    width={120}
-                    draggable={false}
-                    // priority
-                    alt="Euro Speed"
-                    className=""
-                />
-                <p className="text-xs italic text-gray-500">Coming soon</p>
+            <div className="absolute bottom-2 left-2 flex flex-wrap items-end gap-3">
+                <div title="Hot Wheels Premium Euro Speed set coming soon" className="hero-img h-fit bg-white border shadow-md rounded-3xl p-2 flex flex-col items-center">
+                    <Image
+                        src={EuroSpeed}
+                        width={120}
+                        draggable={false}
+                        // priority
+                        alt="Euro Speed"
+                        className=""
+                    />
+                    <p className="text-xs italic text-gray-500">Coming soon</p>
+                </div>
+                <div title="Hot Wheels Premium Exotic Envy set coming soon" className="hero-img h-fit bg-white border shadow-md rounded-3xl p-2 gap-1 flex flex-col items-center">
+                    <Image
+                        src={ExoticEnvy}
+                        width={120}
+                        draggable={false}
+                        // priority
+                        alt="Exotic Envy"
+                        className=""
+                    />
+                    <p className="text-xs italic text-gray-500">Coming soon</p>
+                </div>
             </div>
+
         </div>
     );
 }
