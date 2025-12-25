6f9e08921fe2b1912154df946ec79c380ea5857f
Shubhankar <shubhankartrivedi1234@gmail.com>
2025-12-26 02:44:34 +0530

    razorpay payment notes

--- storefront/src/modules/checkout/components/payment-button/razorpay-payment-button.tsx
+++ storefront/src/modules/checkout/components/payment-button/razorpay-payment-button.tsx
@@ -56,10 +56,10 @@ export const RazorpayPaymentButton = ({
 
 
             image: "https://checkered.in/images/logo.png",
-            // notes: {
-            //     session_id: session.id,
-            //     cart_id: cart.id,
-            // } as any,
+            notes: {
+                session_id: session.id,
+                cart_id: cart.id,
+            } as any,
             modal: {
                 backdropclose: true,
                 escape: true,
