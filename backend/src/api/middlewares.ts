80814f1d1118da46852234a2d74f4ac1526398d5
Shubhankar <shubhankartrivedi1234@gmail.com>
2025-12-27 03:54:03 +0530

    middleware updated

--- backend/src/api/middlewares.ts
+++ backend/src/api/middlewares.ts
@@ -1,17 +1,54 @@
-import { defineMiddlewares, validateAndTransformQuery } from '@medusajs/framework/http';
+import cors from "cors"
+import { defineMiddlewares, validateAndTransformQuery } from "@medusajs/framework/http"
+import { parseCorsOrigins } from "@medusajs/framework/utils"
+import type {
+    MedusaNextFunction,
+    MedusaRequest,
+    MedusaResponse,
+} from "@medusajs/framework/http"
+import { ConfigModule } from "@medusajs/framework/types"
 import { z } from "zod"
 
 export default defineMiddlewares({
     routes: [
+        /**
+         * ===========================
+         *  CORS for /public and /custom-routes
+         * ===========================
+         */
+        {
+            matcher: "/public*",
+            middlewares: [
+                (req: MedusaRequest, res: MedusaResponse, next: MedusaNextFunction) => {
+                    const configModule: ConfigModule = req.scope.resolve("configModule")
+
+                    return cors({
+                        origin: parseCorsOrigins(
+                            configModule.projectConfig.http.storeCors
+                        ),
+                        credentials: false,
+                    })(req, res, next)
+                },
+            ],
+        },
+
+        /**
+         * ===========================
+         *  Validation for /product-feed
+         * ===========================
+         */
         {
             matcher: "/product-feed",
             methods: ["GET"],
             middlewares: [
-                validateAndTransformQuery(z.object({
-                    currency_code: z.string(),
-                    country_code: z.string(),
-                }), {})
-            ]
-        }
-    ]
+                validateAndTransformQuery(
+                    z.object({
+                        currency_code: z.string(),
+                        country_code: z.string(),
+                    }),
+                    {}
+                ),
+            ],
+        },
+    ],
 })
