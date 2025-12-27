01a90c6960d98df35fe34b659dc44a3b88d72ced
Shubhankar <shubhankartrivedi1234@gmail.com>
2025-12-28 02:57:59 +0530

    replyTo added

--- backend/src/modules/email-notifications/services/resend.ts
+++ backend/src/modules/email-notifications/services/resend.ts
@@ -77,7 +77,7 @@ export class ResendNotificationService extends AbstractNotificationProviderServi
       react: emailContent,
       subject: emailOptions.subject ?? 'You have a new notification',
       headers: emailOptions.headers,
-      replyTo: "hello@checkered.in",
+      replyTo: emailOptions.replyTo,
       cc: emailOptions.cc,
       bcc: emailOptions.bcc,
       tags: emailOptions.tags,
