97b1912122d9af9e205e2337f68158c3d9d50a87
Shubhankar <shubhankartrivedi1234@gmail.com>
2026-01-03 17:44:11 +0530

    remove loyality from cart backend

--- backend/src/api/store/carts/[id]/loyalty-points/route.ts
+++ backend/src/api/store/carts/[id]/loyalty-points/route.ts
@@ -1,5 +1,6 @@
 import { MedusaRequest, MedusaResponse } from "@medusajs/framework/http"
 import { applyLoyaltyOnCartWorkflow } from "../../../../../workflows/apply-loyalty-on-cart"
+import { removeLoyaltyFromCartWorkflow } from "src/workflows/remove-loyalty-from-cart"
 
 export async function POST(
     req: MedusaRequest,
@@ -8,6 +9,22 @@ export async function POST(
     const { id: cart_id } = req.params
 
     const { result: cart } = await applyLoyaltyOnCartWorkflow(req.scope)
+        .run({
+            input: {
+                cart_id,
+            },
+        })
+
+    res.json({ cart })
+}
+
+export async function DELETE(
+    req: MedusaRequest,
+    res: MedusaResponse
+) {
+    const { id: cart_id } = req.params
+
+    const { result: cart } = await removeLoyaltyFromCartWorkflow(req.scope)
         .run({
             input: {
                 cart_id,
