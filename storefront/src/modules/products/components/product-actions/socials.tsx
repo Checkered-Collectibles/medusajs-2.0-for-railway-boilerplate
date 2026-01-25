52cc1307ded1c93d57ef8ff16a5e18137e81ab5a
Shubhankar <shubhankartrivedi1234@gmail.com>
2026-01-26 04:01:18 +0530

    added whatsapp lead of product page

--- storefront/src/modules/products/components/product-actions/socials.tsx
+++ storefront/src/modules/products/components/product-actions/socials.tsx
@@ -1,11 +1,20 @@
+"use client";
+
 import Image from "next/image";
 import Link from "next/link";
 import WhatsappLogo from "@images/whatsapp/logo-white.png";
+import { metaEvent } from "@lib/meta/fpixel";
 
 export default function Socials() {
+    const handleJoin = () => {
+        metaEvent("Lead", {
+            content_name: "WhatsApp Group",
+            content_category: "Community"
+        })
+    }
     return (
         <div className="mt-1 text-sm">
-            <Link href="https://chat.whatsapp.com/D09sAJ51EXX5MzuFOAmAAK?mode=hqrt2" title="Join our Whatsapp Group" target="_blank"
+            <Link onClick={handleJoin} href="https://chat.whatsapp.com/D09sAJ51EXX5MzuFOAmAAK?mode=hqrt2" title="Join our Whatsapp Group" target="_blank"
                 className="flex gap-2 items-center justify-center px-3 py-2 bg-[#128c7e] rounded-lg border text-white border-black/10">
                 <Image src={WhatsappLogo} alt="whatsapp" width={30} className="" /> Join our Whatsapp group
             </Link>
