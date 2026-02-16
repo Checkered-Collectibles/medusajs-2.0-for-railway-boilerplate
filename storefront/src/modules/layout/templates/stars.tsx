2bb0ee5e93349b80977c45defbc4471ae1efb730
Shubhankar <shubhankartrivedi1234@gmail.com>
2026-02-17 00:01:00 +0530

    updated stars

--- storefront/src/modules/layout/templates/stars.tsx
+++ storefront/src/modules/layout/templates/stars.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 export default function ReviewStars() {
     return (
         <LocalizedClientLink href="https://www.trustpilot.com/review/checkered.in" target="_blank" className="h-full flex flex-col items-center gap-1.5 text-xs text-green-700 italic">
-            <Image className="CDS_StarRating_starRating__614d2e" alt="TrustScore 4 out of 5" width={108} height={30} src="https://cdn.trustpilot.net/brand-assets/4.1.0/stars/stars-4.svg"></Image>
+            <Image className="CDS_StarRating_starRating__614d2e" alt="TrustScore 4 out of 5" width={108} height={30} src="https://cdn.trustpilot.net/brand-assets/4.1.0/stars/stars-4.5.svg"></Image>
             Trusted by Collectors
         </LocalizedClientLink>
     )
